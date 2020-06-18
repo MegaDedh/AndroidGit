e194613fe556e8690e3ece92ace20c556ff0c215
MegaDedh <MegaDedh@yandex.ru>
2020-06-18 14:55:09 +0700

    commit22

--- app/src/main/java/ru/asshands/softwire/androidgit/MainActivity.kt
+++ app/src/main/java/ru/asshands/softwire/androidgit/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //some changes #1
-        //some changes #2
+        //some changes #22
         //some changes #3
         //some changes #4
         //some changes #5
